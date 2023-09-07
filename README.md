0323d9da4bc253672337e8c712bf01a0cf7842ac
hojinlee <infohojin@gmail.com>
2023-09-07 17:27:25 +0900

    build make command

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # jinyPHP Build
 라라벨 기반의 확장한 모듈입니다.
+
+## 정적 빌드
+
+```
+php artisan build:make
+```
--- src/Console/Commands/BuildMake.php
+++ src/Console/Commands/BuildMake.php
@@ -43,6 +43,27 @@ class BuildMake extends Command
     {
         $this->info("Static Build");
 
+        // 출력폴더 생성
+        $path = base_path().DIRECTORY_SEPARATOR."docs";
+        if(!is_dir($path)) {
+            $this->info("creating docs folder");
+            mkdir($path);
+        }
+
+
+        $path = resource_path("views".DIRECTORY_SEPARATOR."pages");
+        $this->scanDir($path);
+
+        // 출력
+        /*
+        $obj = view("welcome");
+        $body = $obj->__toString();
+        $body = str_replace("http://localhost/","/",$body);
+        file_put_contents($path.DIRECTORY_SEPARATOR."welcom".".html", $body);
+        */
+
+
+
         /*
         $email = $this->argument('email');
 
@@ -62,6 +83,86 @@ class BuildMake extends Command
         }
         */
 
+        return 0;
+    }
+
+    private function scanDir($path)
+    {
+        $pagePath = resource_path("views".DIRECTORY_SEPARATOR."pages");
+
+        $dir = scandir($path);
+        foreach($dir as $name) {
+            if($name == "." || $name == "..") continue;
+            // 디렉터리인 경우, 재귀호출
+            if(is_dir($path.DIRECTORY_SEPARATOR.$name)) {
+                $this->scanDir($path.DIRECTORY_SEPARATOR.$name);
+            } else
+            // 파일인 경우
+            {
+                $filename = str_replace($pagePath,"",$path.DIRECTORY_SEPARATOR.$name);
+                $this->info($filename);
+
+                $fileinfo = pathinfo($path.DIRECTORY_SEPARATOR.$name);
+                //dump($fileinfo);
+                $filePath = str_replace($pagePath,"",$fileinfo['dirname']);
+                //dump($filePath);
+
+                // 디렉터리가 없는 경우 생성
+                $base = base_path("docs");
+
+
+                if(!is_dir($base.$filePath)) {
+                    //dd($base.$filePath);
+                    mkdir($base.$filePath,777, true);
+                }
+
+
+                $ext = $fileinfo['extension'];
+                switch($ext) {
+                    case "php":
+                        $filename = ltrim($filename,"\\");
+                        $filename = str_replace(".blade.php","",$filename);
+                        $filename = str_replace(["\\","/"],".",$filename);
+
+                        //dd($filename);
+                        $obj = view("pages.".$filename);
+                        $body = $obj->__toString();
+                        $body = str_replace("http://localhost/","/",$body);
+                        //dd($base.$filePath.DIRECTORY_SEPARATOR.$filename.".html");
+                        file_put_contents($base.$filePath.DIRECTORY_SEPARATOR.$filename.".html", $body);
+
+                        break;
+                    case "md":
+
+                        $filename = ltrim($filename,"\\");
+                        $filename = str_replace(".md","",$filename);
+
+
+
+                        //dd($filename);
+                        //$obj = view("pages.".$filename);
+                        //$body = $obj->__toString();
+                        //$body = str_replace("http://localhost/","/",$body);
+                        //dd($base.$filePath.DIRECTORY_SEPARATOR.$filename.".html");
+
+                        //dd($path.DIRECTORY_SEPARATOR.$name);
+                        $markdown = file_get_contents($path.DIRECTORY_SEPARATOR.$name);
+
+                        // 마크다운 변환
+                        $Parsedown = new \Parsedown();
+                        $html = $Parsedown->text($markdown);
+
+                        //dd($base.DIRECTORY_SEPARATOR.$filename.".html");
+                        file_put_contents($base.DIRECTORY_SEPARATOR.$filename.".html", $html);
+
+
+                        break;
+                }
+
+
+
+            }
+        }
         return 0;
     }
 
